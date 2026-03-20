21e45187d6ab6a863cd82b389d2923bd7ac6f91d
notsoworkingbro <workingamap@gmail.com>
2026-03-20 23:56:09 +0800

    PreviewPanel feature

--- components/resume/PreviewPanel.tsx
+++ components/resume/PreviewPanel.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import TemplateModern from "./templates/TemplateModern"
+import TemplateMinimal from "./templates/TemplateMinimal"
+import { ResumeData } from "@/lib/types"
 
-const PreviewPanel = () => {
-  return (
-    <div>PreviewPanel</div>
-  )
+const templateMap: { [key: string]: React.ComponentType<{ data: ResumeData }> } = {
+  modern: TemplateModern,
+  minimal: TemplateMinimal,
 }
 
-export default PreviewPanel
+export default function PreviewPanel({
+  data,
+  template,
+}: {
+  data: ResumeData
+  template: string
+}) {
+  const Component = templateMap[template] || TemplateModern
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-[794px] min-h-[1123px] bg-white shadow p-6">
+        <Component data={data} />
+      </div>
+    </div>
+  )
+}
