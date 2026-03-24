03a3c853485f7d39d7677a8e5c71a1beac9c5c80
notsoworkingbro <workingamap@gmail.com>
2026-03-24 18:45:25 +0800

    updated data types skills list, experience dict education dict certificates list

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,28 @@
 type ResumeData = {
   name: string
+  address: string
   email: string
-  skills: string
-  experience: string
-  education: string
-  certificates: string
+  phone: string
+  linkedin: string
+  portfolio: string
+  github: string
+  summary: string
+
+  skills: string[]
+  experience: {
+    company: string
+    duration: string
+    description: string
+  }[]
+
+  education: {
+    school: string
+    duration: string
+    degree: string
+    info: string
+  }[]
+
+  certificates: string[]
 }
 
 export type { ResumeData }
